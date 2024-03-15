c5f074464638f733e2da82429908e82ca5deb2a6
sarthak <sarthak123parajuli@gmail.com>
2024-03-15 13:43:52 +0545

    readme update

--- README.md
+++ README.md
@@ -1,16 +1,78 @@
-# random_visuals
+---
 
-A new Flutter project.
+# Random Event Visualizer
+
+## Overview
+
+This Flutter project is designed to visualize random events such as coin tosses, dice rolls, and potentially other events in the future. The application provides a simple and intuitive interface for users to observe the outcomes of these random events in real-time, along with the frequency distribution of outcomes. 
+
+I've done nothing fancy. Just simple project to visualize outcomes. 
+
+## Features
+
+- **Coin Toss Visualization**: View the results of simulated coin tosses displayed as a visual grid.
+- **Dice Roll Visualization**: Observe the outcomes of simulated dice rolls represented graphically.
+- **Slider Control**: Adjust the number of simulated outcomes using a slider control.
+- **Frequency Distribution**: View the frequency distribution of outcomes in a graphical chart.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+To run this project locally, follow these steps:
 
-A few resources to get you started if this is your first Flutter project:
+1. Clone this repository to your local machine:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+    ```
+    https://github.com/kingace2056/random_event_visualizer.git
+    ```
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+2. Navigate to the project directory:
+
+    ```
+    cd random_event_visualizer
+    ```
+
+3. Ensure you have Flutter installed. If not, follow the [Flutter installation instructions](https://flutter.dev/docs/get-started/install).
+
+4. Run the Flutter project:
+
+    ```
+    flutter run
+    ```
+
+5. Explore the application on your device or emulator.
+
+## Usage
+
+1. Upon launching the application, you will see the main screen with options for visualizing coin tosses and dice rolls.
+
+2. Adjust the number of simulated outcomes using the slider control. The outcomes will update in real-time as you adjust the slider.
+
+3. Visualize the outcomes of coin tosses and dice rolls in their respective sections of the screen.
+
+4. Observe the frequency distribution of outcomes in the chart displayed below the visualizations.
+
+5. Experiment with different numbers of outcomes and observe how the frequency distribution changes.
+
+## Future Enhancements
+
+- **Additional Random Events**: Expand the application to include visualization for other random events, such as card draws, roulette spins, etc.
+- **Customization Options**: Provide options for users to customize the appearance and behavior of visualizations.
+- **Performance Optimization**: Implement optimizations to enhance the performance of the application, especially for a large number of simulated outcomes.
+- **UI/UX Improvements**: Continuously improve the user interface and experience based on feedback and usability testing.
+
+## Contributing
+
+Contributions to this project are welcome! If you have any ideas, feature requests, or bug reports, please feel free to open an issue or submit a pull request.
+
+## Credit
+
+This project was inspired by a TikTok video by Steven Novak username @omnisteven . [View the video](https://vt.tiktok.com/ZSFa11md3/) 
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+Feel free to customize this README file further to include additional details or instructions specific to your project.
--- lib/views/diceroll_view.dart
+++ lib/views/diceroll_view.dart
@@ -1,7 +1,7 @@
-import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class DicerollView extends StatefulWidget {
   const DicerollView({super.key});
@@ -12,14 +12,16 @@ class DicerollView extends StatefulWidget {
 
 class DicerollViewState extends State<DicerollView> {
   int noOfTosses = 100;
+  List<int> dice = [1, 2, 3, 4, 5, 6];
   List<int> cointosses = [];
+  Map<int, int> freqMap = {};
   List<Color> shades = [
-    Color(0xFF000000),
-    Color.fromARGB(207, 0, 0, 0),
-    Color.fromARGB(152, 0, 0, 0),
-    Color.fromARGB(134, 0, 0, 0),
-    Color.fromARGB(69, 0, 0, 0),
-    Color.fromARGB(0, 0, 0, 0)
+    const Color(0xFF000000),
+    const Color.fromARGB(207, 0, 0, 0),
+    const Color.fromARGB(152, 0, 0, 0),
+    const Color.fromARGB(134, 0, 0, 0),
+    const Color.fromARGB(69, 0, 0, 0),
+    const Color.fromARGB(0, 0, 0, 0)
   ];
 
   @override
@@ -33,13 +35,25 @@ class DicerollViewState extends State<DicerollView> {
   emulateCointoss() {
     // simulate 64 coin tosses and store the result in the cointosses list
     for (var i = 0; i < noOfTosses; i++) {
-      Future.delayed(Duration(seconds: 1), () {
+      Future.delayed(const Duration(seconds: 1), () {
         cointosses.add(Random().nextInt(6));
+        frequencyMap();
         setState(() {
           cointosses = cointosses;
         });
       });
     }
+  }
+
+  frequencyMap() {
+    freqMap = {
+      1: cointosses.where((element) => element == 0).length,
+      2: cointosses.where((element) => element == 1).length,
+      3: cointosses.where((element) => element == 2).length,
+      4: cointosses.where((element) => element == 3).length,
+      5: cointosses.where((element) => element == 4).length,
+      6: cointosses.where((element) => element == 5).length,
+    };
   }
 
   @override
@@ -58,34 +72,75 @@ class DicerollViewState extends State<DicerollView> {
                       ? MediaQuery.of(context).size.height
                       : MediaQuery.of(context).size.width,
                   width: MediaQuery.of(context).size.width * 0.7,
-                  child: GridView.builder(
-                    itemCount: cointosses.isNotEmpty ? cointosses.length : 1,
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                        maxCrossAxisExtent: MediaQuery.of(context).size.width /
-                            sqrt(cointosses.length).ceil() /
-                            2,
-                        crossAxisSpacing: 0,
-                        mainAxisSpacing: 0,
-                        childAspectRatio: 1.0),
-                    itemBuilder: ((context, index) => cointosses.isNotEmpty
-                        ? Container(
-                            height: MediaQuery.of(context).size.width / 8 / 2.5,
-                            decoration: BoxDecoration(
-                              color: shades[cointosses[index]],
-                            ),
-                          )
-                        : Text('No emulation yet')),
-                  ),
+                  // child: GridView.builder(
+                  //   itemCount: cointosses.isNotEmpty ? cointosses.length : 1,
+                  //   shrinkWrap: true,
+                  //   physics: NeverScrollableScrollPhysics(),
+                  //   gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  //       maxCrossAxisExtent: MediaQuery.of(context).size.width /
+                  //           sqrt(cointosses.length).ceil() /
+                  //           2,
+                  //       crossAxisSpacing: 0,
+                  //       mainAxisSpacing: 0,
+                  //       childAspectRatio: 1.0),
+                  //   itemBuilder: ((context, index) => cointosses.isNotEmpty
+                  //       ? Container(
+                  //           height: MediaQuery.of(context).size.width / 8 / 2.5,
+                  //           decoration: BoxDecoration(
+                  //             color: shades[cointosses[index]],
+                  //           ),
+                  //         )
+                  //       : Text('No emulation yet')),
+                  // ),
+                  child: Wrap(
+                      children: cointosses.isNotEmpty
+                          ? cointosses
+                              .map((e) => Container(
+                                    height: MediaQuery.of(context).size.width /
+                                        sqrt(noOfTosses).ceil(),
+                                    width: MediaQuery.of(context).size.width /
+                                        sqrt(noOfTosses).ceil(),
+                                    decoration: BoxDecoration(
+                                      color: shades[e],
+                                    ),
+                                  ))
+                              .toList()
+                          : [const Text('No emulation yet')]),
                 ),
                 Column(
                   children: [
-                    Text('Total Tosses: ${cointosses.length}'),
+                    // Text('Total Tosses: ${cointosses.length}'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    const Text('Dice ROll Distribution Graph'),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      child: SfCartesianChart(
+                        series: <CartesianSeries>[
+                          ColumnSeries<int, int>(
+                            dataSource: cointosses,
+                            xValueMapper: (int index, _) => index,
+                            yValueMapper: (int index, _) => freqMap[index + 1],
+                          )
+                        ],
+                      ),
+                    ),
+
+                    Text('Dice Rolls: ${cointosses.length}'),
                     Text(
-                        'Heads: ${cointosses.where((element) => element == 0).length}'),
+                        'Ones: ${cointosses.where((element) => element == 0).length}'),
                     Text(
-                        'Tails: ${cointosses.where((element) => element == 1).length}'),
+                        'Twos: ${cointosses.where((element) => element == 1).length}'),
+                    Text(
+                        'Threes: ${cointosses.where((element) => element == 2).length}'),
+                    Text(
+                        'Fours: ${cointosses.where((element) => element == 3).length}'),
+                    Text(
+                        'Fives: ${cointosses.where((element) => element == 4).length}'),
+                    Text(
+                        'Sixes: ${cointosses.where((element) => element == 5).length}'),
 
                     // show a slider to set number of tosses and a button to emulate the tosses
                     Slider(
@@ -104,8 +159,45 @@ class DicerollViewState extends State<DicerollView> {
                         cointosses = [];
                         emulateCointoss();
                       },
-                      child: Text('Emulate Dice ROlls'),
+                      child: const Text('Emulate Dice ROlls'),
                     ),
+                    Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.red.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          children: [
+                            const Text('Each color represents a dice roll'),
+                            Column(
+                              children: shades
+                                  .map((e) => Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                            height: 20,
+                                            width: 20,
+                                            color: e == const Color(0x00000000)
+                                                ? Colors.white
+                                                : e,
+                                          ),
+                                          const SizedBox(
+                                            width: 10,
+                                          ),
+                                          // map the index of color to dice roll ie from 1 to 6 based on the idnex
+                                          Text((shades.indexOf(e) + 1)
+                                              .toString())
+                                        ],
+                                      ))
+                                  .toList(),
+                            ),
+                          ],
+                        ))
                   ],
                 )
               ],
